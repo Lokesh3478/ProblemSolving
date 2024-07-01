ac9b92549cb3f06adbe7d62579cfcf3bc0eb81ad
Lokesh <119473913+Lokesh3478@users.noreply.github.com>
2024-07-01 21:10:00 +0530

    27.6.24 DC LeetCode 1791

--- LeetCode Dc/June 2024/FindCenterofStarGraph.java
+++ LeetCode Dc/June 2024/FindCenterofStarGraph.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findCenter(int[][] edges) {
-      //A star graph have a single node connected to all other nodes without cycle,
+      //A star graph have a single node connected to all other nodes without cycle, 
       //Therfore the centre node is the node that have all edges connected to it 
       //So return the node that appears common in first two edges
         return (edges[0][0] ==edges[1][0]||
